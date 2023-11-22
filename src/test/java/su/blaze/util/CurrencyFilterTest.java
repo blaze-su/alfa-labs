c233e46c7ad29e221b88ee6ecacf39312a479a27
Сидоров Александр Владимирович <ASidorov7@alfabank.ru>
2023-11-22 21:43:34 +0300

    test(*): add CurrencyFilter

---
+++ src/test/java/su/blaze/util/CurrencyFilterTest.java
@@ -0,0 +1,58 @@
+package su.blaze.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import su.blaze.model.CryptoCurrencyDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyFilterTest {
+
+    private List<CryptoCurrencyDTO> list;
+
+    @BeforeEach
+    void setUp() {
+        list = new ArrayList<>();
+        list.add(new CryptoCurrencyDTO("BTC", "Bitcoin", "", "", ""));
+        list.add(new CryptoCurrencyDTO("ETH", "Ethereum", "", "", ""));
+        list.add(new CryptoCurrencyDTO("DOGE", "Dogecoin", "", "", ""));
+    }
+
+    @Test
+    void testFilterByLabelPartCase() {
+        List<CryptoCurrencyDTO> result = CurrencyFilter.filterByLabel(list, "coin");
+        assertEquals(2, result.size());
+        assertEquals("BTC", result.get(0).getId());
+        assertEquals("DOGE", result.get(1).getId());
+    }
+
+    @Test
+    void testFilterByLabelExactCase() {
+        List<CryptoCurrencyDTO> result = CurrencyFilter.filterByLabel(list, "Bitcoin");
+        assertEquals(1, result.size());
+        assertEquals("BTC", result.get(0).getId());
+    }
+
+    @Test
+    void testFilterByLabelLowerCase() {
+        List<CryptoCurrencyDTO> result = CurrencyFilter.filterByLabel(list, "bitcoin");
+        assertEquals(1, result.size());
+        assertEquals("BTC", result.get(0).getId());
+    }
+
+    @Test
+    void testFilterByLabelUpperCase() {
+        List<CryptoCurrencyDTO> result = CurrencyFilter.filterByLabel(list, "BITCOIN");
+        assertEquals(1, result.size());
+        assertEquals("BTC", result.get(0).getId());
+    }
+
+    @Test
+    void testFilterByLabelNoMatch() {
+        List<CryptoCurrencyDTO> result = CurrencyFilter.filterByLabel(list, "Litecoin");
+        assertTrue(result.isEmpty());
+    }
+}
